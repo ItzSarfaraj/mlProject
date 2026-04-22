4562d6c444435f85af8809b6ea2d946d85ed5b9e
Sarfaraj Siddiqui <sarfarajsiddiqui@gmail.com>
2026-04-23 00:16:12 +0530

    project structure

---
+++ src/exception.py
@@ -0,0 +1,38 @@
+##Create a custom exception class that gives detailed error info like:
+#File name
+#Line number
+#Actual error message
+
+##Because:
+
+#Normal errors are vague
+#This gives debug-level clarity
+
+import sys
+import logging
+
+def error_message_details(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occur in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name, exc_tb.tb_lineno,str(error)
+    )
+    return error_message    
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message) 
+        self.error_message=error_message_details(error_message,error_detail=error_detail)   
+
+    def __str__(self):
+        return self.error_message    
+    
+
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("divide by zero")
+        raise CustomException(e,sys)    
