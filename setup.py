f74d4b9e56503066a0fe3171654a8637dc8395c6
Sarfaraj Siddiqui <sarfarajsiddiqui@gmail.com>
2026-04-22 23:37:21 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        # remove empty lines
+        requirements = [req for req in requirements if req]
+
+        # remove "-e ."
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="mlproject",
+    version="0.1.0",
+    author="sarfaraj",
+    author_email="sarfarajsiddiqui002@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+) 
